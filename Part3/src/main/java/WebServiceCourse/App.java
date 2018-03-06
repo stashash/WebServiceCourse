c9eaef76ce0583c17b4aed4b0258890165256cd5
stashash <stas_hash@mail.ru>
2018-03-06 22:37:47 +0300

    Add Part3 project

---
+++ Part3/src/main/java/WebServiceCourse/App.java
@@ -0,0 +1,13 @@
+package WebServiceCourse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
